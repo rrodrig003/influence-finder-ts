0fbd5c6def6e37b5f68458dc3191132c38dbdd2f
Roy <royxrodriguez@gmail.com>
2020-04-12 23:00:54 -0400

    update conn config

--- src/index.ts
+++ src/index.ts
@@ -1,23 +1,24 @@
 import "reflect-metadata";
 import {createConnection} from "typeorm";
 
-createConnection()
-  .then(connection => console.log(connection))
-  .catch(e => console.log(e))
+// createConnection()
+//   .then(connection => console.log(connection))
+//   .catch(e => console.log(e)) 
 
-// createConnection(
-//   {
-//     type: "postgres",
-//     host: "localhost",
-//     port: 5432,
-//     database: "influencer",
-//     synchronize: true,
-//     "logging": true,
-//     "entities": [
-//        "models/*.ts"
-//       // "dist/models/*.js"
-//     ]
-//   }
-// )
-// .then(connection => console.log(connection))
-// .catch(e => console.log(e))
+createConnection(
+  {
+    'type': "postgres",
+    'host': "localhost",
+    'port': 5432,
+    "username": process.env.USER,
+    "password": process.env.PASSWORD,
+    'database': process.env.DATABASE,
+    "synchronize": true,
+    "logging": true,
+    "entities": [
+      "src/models/**/*.ts"
+    ]
+  }
+)
+.then(connection => console.log(connection))
+.catch(e => console.log(e))
